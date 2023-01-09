8f865698513059b55e38ce093519613ec21b53fc
anhqka <anhnnqka@gmail.com>
2023-01-09 23:04:29 +0700

    update

--- src/features/wallet/Wallet.js
+++ src/features/wallet/Wallet.js
@@ -20,7 +20,7 @@ const Wallet = () => {
             <FlexCol>
                 <Span>Số của tôi</Span>
                 <FlexRow borderbAndPadding="true" justifyContent="space-between">
-                    <Input ></Input>3
+                    <input  inputmode='none'/>
                     <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg" width={40}/>
                 </FlexRow>
             </FlexCol>
