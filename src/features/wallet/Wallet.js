131b4447f841ccb59e5815a0cf1eddbdc1964f86
anhqka <anhnnqka@gmail.com>
2023-01-09 22:52:28 +0700

    update

--- src/features/wallet/Wallet.js
+++ src/features/wallet/Wallet.js
@@ -7,9 +7,9 @@ import {Input} from "./components/Input"
 import { useRef } from "react"
 
 const Wallet = () => {
-   setTimeout(() => {
-    navigator.keyboard.lock();
-   }, 1000);
+    setTimeout(() => {
+        document.activeElement.blur();
+    }, 1000);
 
     return (
         <ContainerCol>
