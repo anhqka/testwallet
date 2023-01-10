3430a0c045a67384d77c4676c218ee207bf86fa7
Anh Nhật <anhnnqka@gmail.com>
2023-01-11 03:46:36 +0700

    update

--- src/features/wallet/components/TopTabs.js
+++ src/features/wallet/components/TopTabs.js
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 export const TopTabs = styled.button`
   border: none;
-  background-color: #015A80;
-  color: #19D6FF;
   font-size: 14px;
   font-weight: 600;
   padding: 8px 16px;
   border-radius: 16px;
+  ${({ bg }) => bg
+    ? `
+         background-color: #015A80;
+         color:  #19D6FF;
+         
+    `
+    : `
+      background-color: #013E58;
+      color: #15A0BE;
+    `}
 `;
--- src/features/wallet/components/icon/WalletProviderIcon.js
+++ src/features/wallet/components/icon/WalletProviderIcon.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
-import { CheckOutlined } from "@ant-design/icons"
 
-export const WalletProviderIcon = styled(CheckOutlined)`
+export const WalletProviderIcon = styled.img.attrs(({ src }) => ({
+  src: src
+}))`
   font-size: 9px;
   position: absolute;
   right:0;
   top:0;
   border-bottom-left-radius: 30px;
   border-top-right-radius: 9px;
-  padding: 6px;
+  padding: 5px;
   background-color: #19D6FF;
+  width: ${({ w }) => w};
+    height: ${({ h }) => h};
 `;
